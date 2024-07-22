d4637cfd95bb12088814e1f55cd4f81045a238b4
LokeshKuruba <140333595+LokeshKuruba@users.noreply.github.com>
2024-07-22 21:55:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+let bgcolor = document.getElementById("colorPickerContainer");
+let selectedColorHexCodeEl = document.getElementById("selectedColorHexCode");
+
+function button11() {
+    bgcolor.style.backgroundColor = "#e0e0e0";
+    selectedColorHexCodeEl.textContent = "#e0e0e0";
+    selectedColorHexCodeEl.style.color = "#49a6e9";
+}
+
+function button22() {
+    bgcolor.style.backgroundColor = "#6fcf97";
+    selectedColorHexCodeEl.textContent = "#6fcf97";
+    selectedColorHexCodeEl.style.color = "#49a6e9";
+}
+
+function button33() {
+    bgcolor.style.backgroundColor = "#56ccf2";
+    selectedColorHexCodeEl.textContent = "#56ccf2";
+    selectedColorHexCodeEl.style.color = "#49a6e9";
+}
+
+function button44() {
+    bgcolor.style.backgroundColor = "#bb6bd9";
+    selectedColorHexCodeEl.textContent = "#bb6bd9";
+    selectedColorHexCodeEl.style.color = "#49a6e9";
+}
